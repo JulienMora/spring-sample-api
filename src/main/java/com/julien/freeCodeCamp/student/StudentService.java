883e644f1256431d81d0b0186d14db752a4c032a
Julien Moratalla <julien.moratalla@gmail.com>
2021-06-05 20:33:33 +0200

    email

--- src/main/java/com/julien/freeCodeCamp/student/StudentService.java
+++ src/main/java/com/julien/freeCodeCamp/student/StudentService.java
@@ -2,10 +2,8 @@ package com.julien.freeCodeCamp.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -19,5 +17,14 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository
+                .findStudentByEmail(student.getEmail());
+        if (studentByEmail.isPresent()) {
+            throw new IllegalStateException("email taken !");
+        }
+        System.out.println(student);
     }
 }
